6a0d84aa12f48f85cd5dbe373cf0df5862d0ce05
Administrator <admin@example.com>
2020-05-26 15:03:35 +0200

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello World");
+		get("/oficintecnica", (req, res) -> "Esto es una prueba 26/11/2020");
 	}
 }
